95959cd8ec89964533d5733322ccd3a243572031
Mrunalee Patel <mrunaleepatel997@gmail.com>
2023-05-24 20:40:21 -0500

    new jquery test

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
-$.ajax("https://api.mfapi.in/mf/100066")
+// $.ajax("https://api.mfapi.in/mf")
+//     .then((data) => {
+//         console.log(data)
+//         console.log
+//     })
+
+    $.ajax("https://api.mfapi.in/mf/100119")
     .then((data) => {
         console.log(data)
-        console.log(data.meta.scheme_name)
+        console.log
     })
+
+    // $.ajax("https://api.artic.edu/api/v1/artworks")
+    // .then((data) => {
+    //     console.log(data)
+    //     console.log
+    // })
+    
