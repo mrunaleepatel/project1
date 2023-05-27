a814c3757123637356507fd9741cac2b78c3009c
Mrunalee Patel <mrunaleepatel997@gmail.com>
2023-05-26 23:08:54 -0500

    homepage

--- app.js
+++ app.js
@@ -1,18 +1,43 @@
-// $.ajax("https://api.mfapi.in/mf")
-//     .then((data) => {
-//         console.log(data)
-//         console.log
-//     })
+const departmentsUrl = "https://collectionapi.metmuseum.org/public/collection/v1/departments"
+const objectsUrl = "https://collectionapi.metmuseum.org/public/collection/v1/search?q="
+console.log(departmentsURL)
 
-    $.ajax("https://api.mfapi.in/mf/100119")
-    .then((data) => {
-        console.log(data)
-        console.log
-    })
+function getDepartments() {
+    $.ajax(departmentsUrl)
+    .then((response) => {
+        console.log(response);
+        const objectIDs = response.objectIDs.slice(0, 10);
+        const $main = $("main");
+        $main.empty();
+        objectIDs.forEach((objectID) => {
+            const url = `${objectsUrl}/${objectID}`;
+            console.log(url);
+            $.ajax(url)
+            .then((object) => {
+                console.log(object);
+                const div = $("<div>");
+                div.html(`
+                <img src="${object.primaryImage}">
+                <h2>${object.medium}</h2>
+                <h2>${object.dynasty}</h2>
+                <h2>${object.culture}</h2>
+                <h2>${object.title}
+                <h2>${object.department}</h2>
+                `);
+                $main.append(div);
+            });
+        });
+    });
+}
 
-    // $.ajax("https://api.artic.edu/api/v1/artworks")
-    // .then((data) => {
-    //     console.log(data)
-    //     console.log
-    // })
+$("input[type=submit]").on("click", (event)=>
+{   //prevent the refresh
+    event.preventDefault()
+    // grab text from input box
+    //check if the artistDisplayName includes the search keyword,
+    const inputText=$("input[type=text]").val() // jquery val() actually grabs the value or it wont work
+    // update the screen
     
+    getDepartments(inputText)
+})
+
